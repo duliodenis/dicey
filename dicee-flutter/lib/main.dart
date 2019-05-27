7f7c3ffdd83be60763e2ea36800fa51e85efdb26
Dulio Denis <dulio.denis@yahoo.com>
2019-05-26 20:04:12 -0400

    Changed to Stateful and use setState onPressed

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -15,18 +15,26 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDieNumber = 1;
+  int rightDieNumber = 2;
+
   @override
   Widget build(BuildContext context) {
-    var leftDieNumber = 1;
-    var rightDieNumber = 2;
     return Center(
       child: Row(
         children: <Widget>[
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Left die tapped.');
+                setState(() {
+                  leftDieNumber = 5;
+                });
               },
               child: Image.asset('images/dice$leftDieNumber.png'),
             ),
@@ -34,7 +42,9 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: FlatButton(
               onPressed: () {
-                print('Right die tapped.');
+                setState(() {
+                  rightDieNumber = 5;
+                });
               },
               child: Image.asset('images/dice$rightDieNumber.png'),
             ),
